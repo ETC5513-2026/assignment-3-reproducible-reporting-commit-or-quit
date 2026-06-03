b5a5c91fa04981457c8d6cc74c5ce34ebf6d2964
vikywayne <vikash.mm06@gmail.com>
2026-06-03 20:35:39 +1000

    Complete methodology section and workflow figure

--- Scripts/01_data_cleaning.R
+++ Scripts/01_data_cleaning.R
@@ -28,8 +28,7 @@ energy_analysis <- energy_major %>%
     entity,
     year,
     renewable_energy_share_in_the_total_final_energy_consumption_percent,
-    value_co2_emissions_kt_by_country,
-    gdp_per_capita
+    value_co2_emissions_kt_by_country
   )
 
 # Check missing values
