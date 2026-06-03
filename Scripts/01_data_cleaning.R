dfe9e100abc5b904c165ca862c806c52683e01ff
vikywayne <vikash.mm06@gmail.com>
2026-06-03 10:20:56 +1000

    Add cleaned dataset pipeline, visualisations, and summary tables

---
+++ Scripts/01_data_cleaning.R
@@ -0,0 +1,43 @@
+# Load required libraries
+library(tidyverse)
+library(janitor)
+library(skimr)
+
+# Read dataset
+energy <- read_csv("Data/global-data-on-sustainable-energy.csv")
+
+# Clean column names
+energy <- clean_names(energy)
+
+# Filter selected countries
+major_countries <- c(
+  "United States",
+  "China",
+  "India",
+  "Australia"
+)
+
+
+# Select variables for analysis
+energy_major <- energy %>%
+  filter(entity %in% major_countries)
+
+
+energy_analysis <- energy_major %>%
+  select(
+    entity,
+    year,
+    renewable_energy_share_in_the_total_final_energy_consumption_percent,
+    value_co2_emissions_kt_by_country,
+    gdp_per_capita
+  )
+
+# Check missing values
+colSums(is.na(energy_analysis))
+
+
+# Export cleaned dataset
+write_csv(
+  energy_analysis,
+  "Data/energy_analysis.csv"
+)
